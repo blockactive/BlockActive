c78ab72eba2aa02aeb54ee8df247dfc5757c6699
MateoEmilio1 <mateoyastor60@gmail.com>
2024-08-16 17:24:20 -0300

    subgraph working

--- blockactive/src/address-validator.ts
+++ blockactive/src/address-validator.ts
@@ -1,33 +1,12 @@
 import {
   AddACorruptAddress as AddACorruptAddressEvent,
-  RemoveCorruptAddress as RemoveCorruptAddressEvent
-} from "../generated/AddressValidator/AddressValidator"
-import { AddACorruptAddress, RemoveCorruptAddress } from "../generated/schema"
+  RemoveCorruptAddress as RemoveCorruptAddressEvent,
+} from "../generated/AddressValidator/AddressValidator";
+import { CorruptAddress } from "../generated/schema";
 
 export function handleAddACorruptAddress(event: AddACorruptAddressEvent): void {
-  let entity = new AddACorruptAddress(
-    event.transaction.hash.concatI32(event.logIndex.toI32())
-  )
-  entity._address = event.params._address
+  
+  let  entity = new CorruptAddress(event.params._address.toHexString());
 
-  entity.blockNumber = event.block.number
-  entity.blockTimestamp = event.block.timestamp
-  entity.transactionHash = event.transaction.hash
-
-  entity.save()
-}
-
-export function handleRemoveCorruptAddress(
-  event: RemoveCorruptAddressEvent
-): void {
-  let entity = new RemoveCorruptAddress(
-    event.transaction.hash.concatI32(event.logIndex.toI32())
-  )
-  entity._address = event.params._address
-
-  entity.blockNumber = event.block.number
-  entity.blockTimestamp = event.block.timestamp
-  entity.transactionHash = event.transaction.hash
-
-  entity.save()
+  entity.save();
 }
